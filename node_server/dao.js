84140f8d10f171cfd8fc1ec2ef64b15bc3e353d3
Prashant Bajpai <prashant.bajpai21@gmail.com>
2019-04-06 18:21:29 +0530

    added mysql infra

---
+++ node_server/dao.js
@@ -0,0 +1,25 @@
+const PropertiesReader = require('properties-reader');
+const mysql = require('mysql');
+
+var properties = PropertiesReader('./res/db_config.properties');
+
+console.log(properties.get("db.host"));
+
+var pool  = mysql.createPool({
+  connectionLimit : 10,   //default value = 10
+  host            : properties.get("db.host"),
+  user            : properties.get("db.user"),
+  password        : properties.get("db.password"),
+  database        : properties.get("db.name")
+});
+
+pool.getConnection((err, connection) => {
+    if (err) throw err;
+    connection.ping((err) => {
+        connection.release();
+        if (err) throw err;
+        console.log('Server responded to ping');
+      
+    })
+    
+})
