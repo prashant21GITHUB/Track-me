2663768bdb6d4e3f02ac0e04311b3f480df8c1e8
Prashant Bajpai <prashant.bajpai21@gmail.com>
2019-04-17 23:40:16 +0530

    added correct location coords

--- node_server/socket_data_publisher.js
+++ node_server/socket_data_publisher.js
@@ -5,15 +5,15 @@ var socket = io.connect('http://localhost:3000');
 socket.emit('publish', {
     
               mobile: '7767947111',
-              lat: 10.00,
-              lng: 20.89
+              lat: 28.630,
+              lng: 77.365
 
 })
 
 socket.emit('publish', {
     
     mobile: '9540181912',
-    lat: 30.00,
-    lng: 40.89
+    lat: 28.6206,
+    lng: 77.3555
 
 })
