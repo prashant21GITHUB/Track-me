543b493fab75c114c3675f164c00dd7b05843121
EC2 Default User <ec2-user@ip-172-31-29-151.ap-south-1.compute.internal>
2019-05-10 22:54:33 +0000

    added data publisher

--- node_server/socket_data_publisher.js
+++ node_server/socket_data_publisher.js
@@ -3,6 +3,7 @@ var io = require("socket.io-client")
 var socket = io.connect('http://localhost:3000');
 
 socket.emit('startPublish', ['8553016002', '7767947111'])
+socket.emit('startPublish', ['8860090884', '7767947111','9919788200'])
 
 // socket.emit('publish', {
     
@@ -21,10 +22,19 @@ socket.emit('startPublish', ['8553016002', '7767947111'])
 socket.emit('publish', {
     
     mobile: '8553016002',
-    lat: 28.6417,
-    lng: 77.3575
+    lat: 39.913818,
+    lng: 116.3636
 
 })
+
+socket.emit('publish', {
+
+    mobile: '8860090884',
+    lat: 39.923818,
+    lng: 116.3636
+
+})
+
 
 socket.on('7767947111', (data) => {
     console.log(data);
